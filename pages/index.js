7734e709becfbe4becc88142192776b0f1af096f
Themba-M-max <64152381+Themba-M-max@users.noreply.github.com>
2022-11-21 02:50:48 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+
+export default class extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Craig Themba Maphosa</h1>
+
+        <p>Cell: 078 036 6034 </p>
+        <p>Email: craigthembamaphosa77@gmail.com</p>
+        <a href="https://www.linkedin.com/in/themba-maphosa-21994b1a3/">LinkedIn</a>
+
+        <h2>Profile</h2>
+
+        <p>
+           I am recently finished my degree in Computer Science at the University of Cape Town.
+           I would love to work at SovTech because I believe it would provide a great foundation to the start of my career as a Software Engineer.
+           I would love to gain as much experience as possible and I believe that I would be very valuable to the company because as I am very hungry to produce software of industry standard.
+        </p>
+
+
+      </div>
+    )
+  }
+}
